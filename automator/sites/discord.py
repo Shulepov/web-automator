ae30e5591e380f9d599c393722672b11c91676cb
Shulepov <mikhail.shulepov@gmail.com>
2023-02-13 23:23:04 +0800

    Discord: send message

--- automator/sites/discord.py
+++ automator/sites/discord.py
@@ -1,11 +1,41 @@
 from time import sleep
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 class Discord:
     def __init__(self, browser):
-       self.browser = browser
-       self.prev_window = None
-       self.discord_window = None
+        self.browser = browser
+        self.prev_window = None
+        self.discord_window = None
+
+    def open(self, link):
+        if not self.prev_window:
+            self.prev_window = self.browser.get_current_window_handle()
+        if not self.discord_window:
+            self.browser.new_tab(link)
+            sleep(3)
+            self.discord_window = self.browser.get_current_window_handle()
+        else:
+            self.browser.go_to(link)
+        sleep(10)
+    
+    def close(self):
+        self.discord_window = None
+        self.browser.close_current_tab()
+        self.browser.switch_to_window(self.prev_window)
+        self.prev_window = None
 
     def accept_invite(self):
-       return None
+        return None
+
+    def send_message(self, channel, message):
+        self.open(channel)
+        input = self.browser.driver.find_element(By.XPATH, '//div[@role="textbox"]')
+        input.click()
+        input.send_keys(message)
+        sleep(2)
+        input.send_keys(Keys.RETURN)
+        sleep(5)
+        
+        
+
