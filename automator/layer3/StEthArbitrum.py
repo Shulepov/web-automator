15973a1b5b0d3be896be8bb226d6b2f14c2370f0
Shulepov <mikhail.shulepov@gmail.com>
2023-02-13 11:11:41 +0800

    Unsaved changes

--- automator/layer3/StEthArbitrum.py
+++ automator/layer3/StEthArbitrum.py
@@ -19,25 +19,8 @@ def balancer_trade(layer3_window, browser, metamask):
 def balancer_pool(layer3_window, browser, metamask):
 	browser.new_tab("https://app.balancer.fi/#/arbitrum/pool/0xfb5e6d0c1dfed2ba000fbc040ab8df3615ac329c000000000000000000000159/invest")
 	sleep(10)
-	#0xEeee... - it's Token for ETH ()
-	browser.driver.find_element(By.XPATH, '//input[@name="0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"]').send_keys('0.0001')
-	sleep(2)
-	browser.driver.find_element(By.XPATH, '//button[contains(.,"Preview")]').click()
-	sleep(3)
-	browser.driver.find_element(By.XPATH, '//button[contains(.,"Add liquidity")]').click()
-	sleep(5)
-	metamask.confirm_transaction()
-
-	#stake
-	browser.driver.find_element(By.XPATH, '//button[contains(.,"Stake this to earn extra")]').click()
-	sleep(7)
-	browser.driver.find_element(By.XPATH, '//button[contains(.,"Approve")]').click()
-	sleep(5)
-	metamask.confirm_token_approval()
-	browser.driver.find_element(By.XPATH, '//button[contains(.,"Stake")]').click()
-	sleep(5)
-	metamask.confirm_transaction()
-
+	balancer = Balancer(browser, metamask)
+	balancer.add_liquidity("0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE", '0.0001')
 	return_and_verify(browser, layer3_window) #liquidity
 	verify(browser, layer3_window) #staking
 
