bbdc408f32b24120b80db3933259bc0b3c62ea25
Shulepov <mikhail.shulepov@gmail.com>
2023-02-13 11:00:16 +0800

    Refactor StEthArbitrum scenario

--- automator/layer3/StEthArbitrum.py
+++ automator/layer3/StEthArbitrum.py
@@ -2,67 +2,18 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 from .helpers import *
+from .quiz import do_quiz
+from automator.sites import Balancer
 
 def quiz(browser, metamask):
-	browser.click("Start Quiz!")
-	sleep(1)
-	browser.driver.find_element(By.XPATH, '//div[@role="radiogroup"]/button[position()=1]').click()
-	sleep(1)
-	browser.click("Next Question")
-	sleep(1)
-	browser.driver.find_element(By.XPATH, '//div[@role="radiogroup"]/button[position()=1]').click()
-	sleep(1)
-	browser.click("Next Question")
-	sleep(1)
-	browser.driver.find_element(By.XPATH, '//div[@role="radiogroup"]/button[position()=1]').click()
-	sleep(1)
-	browser.click("Next Question")
-	sleep(1)
+	do_quiz(browser, [1, 1, 1])
 
 def balancer_trade(layer3_window, browser, metamask):
 	browser.new_tab("https://app.balancer.fi/#/arbitrum/trade")
 	sleep(10)
-
-	try:
-		browser.driver.find_element(By.XPATH, '//button[contains(.,"Connect wallet")]').click()
-		sleep(2)
-		browser.driver.find_element(By.XPATH, '//button[contains(.,"Metamask") and contains(@class,"wallet-connect-btn")]').click()
-		metamask.connect_to_website()
-		sleep(3)
-	except Exception as e:
-		print(e)
-		print("Looks like already connected to balancer")
-
-	currencies = browser.driver.find_elements(By.XPATH, '//div[contains(@class, "token-select-input")]')
-	if currencies[0].text != "ETH":
-		currencies[0].click()
-		sleep(6)
-		token_search = browser.driver.find_element(By.XPATH, '//input[@name="tokenSearchInput"]')
-		token_search.send_keys('ETH')
-		sleep(2)
-		token_search.send_keys(Keys.RETURN)
-		sleep(2)
-	if currencies[1].text != 'wstETH':
-		currencies[1].click()
-		sleep(2)
-		token_search = browser.driver.find_element(By.XPATH, '//input[@name="tokenSearchInput"]')
-		token_search.send_keys('wstETH')
-		sleep(2)
-		token_search.send_keys(Keys.RETURN)
-		sleep(2)
-	browser.driver.find_element(By.XPATH, '//input[@name="tokenIn"]').send_keys('0.0001')
-	sleep(5)
-	try:
-		browser.driver.find_element(By.XPATH, '//button[contains(.,"Accept")]').click()
-		sleep(2)
-	except:
-		print("No significant price change")
-	browser.driver.find_element(By.XPATH, '//button[contains(.,"Preview")]').click()
-	sleep(3)
-	browser.driver.find_element(By.XPATH, '//button[contains(.,"Confirm swap")]').click()
-	sleep(5)
-	metamask.confirm_transaction()
-
+	balancer = Balancer(browser, metamask)
+	balancer.try_connect()
+	balancer.execute_swap(["ETH", "wstETH"], "0.0001")
 	return_and_verify(browser, layer3_window)
 
 def balancer_pool(layer3_window, browser, metamask):
